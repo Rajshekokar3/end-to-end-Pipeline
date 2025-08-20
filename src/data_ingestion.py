b5f14d287cb56fc116b85b6cacfb354c7d594f45
Raj Shekokar <rajshekokar3@gmail.com>
2025-08-20 19:51:17 +0530

    untill Feature enginerring

---
+++ src/data_ingestion.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import os 
+from sklearn.model_selection import train_test_split
+import logging
+
+#Ensure  the "logs"
+
+log_dir="logs"
+os.makedirs(log_dir,exist_ok=True) # it present or not
+
+#logging configuration 
+logger =logging.getLogger('data_ingestion')
+logger.setLevel("DEBUG")
+
+console_handler =logging.StreamHandler() # it will print in terminal 
+console_handler.setLevel("DEBUG")
+
+#file handler 
+log_file_path=os.path.join(log_dir,"data_ingestion.log")
+file_handler=logging.FileHandler(log_file_path)
+file_handler.setLevel("DEBUG")
+
+
+#Formating 
+formattter=logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+console_handler.setFormatter(formattter)# applying the formatter
+file_handler.setFormatter(formattter) # applying the formatter 
+
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+# data  loading 
+
+def load_data(data_url: str) ->pd.DataFrame:
+    """Load data from CSV file."""
+    try:
+        df=pd.read_csv(data_url)
+        logger.debug('Data loaded from %s',data_url)
+        return df
+    except pd.errors.ParserError as e:
+        logger.error('Failed to parse the CSV file: %s',e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error occured while loading the data: %s',e)
+        raise
+
+
+def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
+    """Preprocess the data """
+    try:
+        df.drop(columns = ['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], inplace = True)
+        df.rename(columns = {'v1': 'target', 'v2': 'text'}, inplace = True)
+        logger.debug("Data preprocessing is cmpleted")
+        return df 
+    
+    except KeyError as e:
+        logger.error('missing colum isn the data frame: %s',e)
+        raise
+    except Exception as e:
+        logger.error('unexpected error during preprocessing: %s',e)
+        raise 
+
+
+def save_data(train_data:pd.DataFrame ,test_data:pd.DataFrame,data_path:str)->None:
+    "Save the train and test dataset"
+
+    try:
+        raw_data_path=os.path.join(data_path,'raw')
+        os.makedirs(raw_data_path,exist_ok=True)
+        train_data.to_csv(os.path.join(raw_data_path,"train.csv"),index=True)
+        test_data.to_csv(os.path.join(raw_data_path,"test.csv"),index=True)
+        logger.debug("Train and test data saged to %s",raw_data_path)
+
+    except Exception as e:
+        logger.error("Unexcepted error occuped while saving the data: %s ",e)
+        raise 
+
+def main():
+    try:
+        test_size=0.2
+        data_path="https://raw.githubusercontent.com/Rajshekokar3/Dataset-/refs/heads/main/spam.csv"
+        df=load_data(data_url=data_path)
+        final_df=preprocess_data(df)
+        train_data,test_data=train_test_split(final_df,test_size=test_size,random_state=42)
+        save_data(train_data,test_data,data_path='./data')
+
+    except Exception as e:
+        logger.error("Failed to  complete dat ingestion process: %s",e)
+        print(f"Error:{e}")
+
+
+if __name__=='__main__':
+    main()
---
+++ src/feature_enginerring.py
@@ -0,0 +1,111 @@
+import os 
+import pandas as pd
+import logging
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+
+log_dir="logs"
+os.makedirs(log_dir,exist_ok=True) # it present or not
+
+#logging configuration 
+logger =logging.getLogger('Feature Engineering')
+logger.setLevel("DEBUG")
+
+console_handler =logging.StreamHandler() # it will print in terminal 
+console_handler.setLevel("DEBUG")
+
+#file handler 
+log_file_path=os.path.join(log_dir,"Feature Enginerring.log")
+file_handler=logging.FileHandler(log_file_path)
+file_handler.setLevel("DEBUG")
+
+
+#Formating 
+formattter=logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+console_handler.setFormatter(formattter)# applying the formatter
+file_handler.setFormatter(formattter) # applying the formatter 
+
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+def load_data(file_path:str)->pd.DataFrame:
+    """
+    load data from csv file
+    """
+    try:
+        df=pd.read_csv(file_path)
+        df.fillna('',inplace=True)
+        logger.debug("Data loadedand nans filled from %s",file_path)
+        return df
+    except pd.errors.ParserError as e:
+        logging.error("Failed to parse th csv file %s",e)
+        raise
+    except Exception as e:
+        logging.error("Unexcepted error occured while looking the data:%s",e)
+        raise 
+
+def apply_tfid(train_data:pd.DataFrame,test_data:pd.DataFrame,max_feature:int)->tuple:
+
+    """
+    Apply tfid to the data
+
+    """
+    try:
+        vectorizer=TfidfVectorizer(max_features=max_feature)
+
+        x_train=train_data['text'].values
+        y_train=train_data['target'].values
+        
+        x_test=test_data['text'].values
+        y_test=test_data['target'].values
+
+        x_train_bow=vectorizer.fit_transform(x_train)
+        x_test_bow=vectorizer.transform(x_test)
+        
+        train_df=pd.DataFrame(x_train_bow.toarray())
+        train_df['label']=y_train
+
+        test_df=pd.DataFrame(x_test_bow.toarray())
+        test_df['label']=y_test
+
+        logger.debug("Bag of words applied and data transformed")
+        return train_df,test_df
+    except Exception as e:
+        logger.error("Error during Bag of Words transformed %s",e)
+
+
+def save_data(df:pd.DataFrame,file_path:str) ->None:
+    """
+      Save the dataframe to a CSV FILE.
+    """
+    try:
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+        df.to_csv(file_path,index=False,mode="w")
+        logger.debug('Data saved to %s',file_path)
+
+    except Exception as e:
+        logger.error("Unexcepted error occured while saving the data %s",e)
+        raise
+
+def main():
+    try:
+        max_feature=50
+
+        train_data=load_data('./data/interim/train_processed_data.csv')
+        test_data=load_data('./data/interim/test_processed_data.csv')
+
+        train_df,test_df=apply_tfid(train_data,test_data,max_feature)
+
+        save_data(train_df,os.path.join("./data","processed","train_tfid.csv"))
+        save_data(test_df,os.path.join("./data","processed","test_tfid.csv"))
+
+    except Exception as e:
+        logger.error("Failed to complete the feature enginerring process: %s",e)
+        print(f"Error",e)
+
+
+if __name__=="__main__":
+    main()
